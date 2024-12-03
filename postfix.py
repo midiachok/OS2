e7404784b8bf3af876367cf07581a83b6db23633
midiachok <162707182+midiachok@users.noreply.github.com>
2024-12-03 14:39:05 +0100

    uprava!

--- postfix.py
+++ postfix.py
@@ -1,19 +1,44 @@
-def eval_expr(s):
-    items=s.split()
+import operator
+
+oper_d={
+        '+':operator.add,
+        '-':operator.sub,
+        '*':operator.mul,
+        '/':operator.floordiv,
+        }
+
+
+def eval_expr(s,**var_d):
     stack=[]
     
-    for item in items:
-        if item in {"+","-","/","*"}:
+    for item in s.split():
+        if item in oper_d:
             a = stack.pop()
             b = stack.pop()
-            if item == "+":
-                stack.append(a+b)
-            elif item == "-":
-                stack.append(b-a)
-            elif item == "/":
-                stack.append(b//a)
-            elif item == "*":
-                stack.append(a*b)
+            stack.append(oper_d[item](a,b))
+        elif item in var_d:
+            stack.append(int(var_d[item]))
         else:
             stack.append(int(item))
-    return stack[0]
+    return stack.pop()
+
+def eval_expr_f(s):
+    stack=[]
+    
+    for item in s.split():
+        if item in oper_d:
+            a_f = stack.pop()
+            b_f = stack.pop()
+            def fun(**var_d):
+                return oper_d[item](a_f(**var_d),b_f(**var_d))
+            stack.append(fun)
+        elif item.isdigit():
+            value=int(item)
+            def fun(**var_d):
+                return value
+            stack.append(fun)
+        else:
+            def fun(**var_d):
+                value = var_d[item]
+            stack.append(fun)
+    return stack.pop()
